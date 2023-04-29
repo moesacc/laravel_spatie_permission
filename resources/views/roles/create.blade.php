6819f14ab26c5b46fb8b084725306903bbcda460
zarnilinhtet <zarnilinhtet1229@gmail.com>
2023-04-29 11:35:29 +0630

    Upload msn table

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -25,6 +25,7 @@
 @endif
 
 {!! Form::open(array('route' => 'roles.store','method'=>'POST')) !!}
+
 <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
